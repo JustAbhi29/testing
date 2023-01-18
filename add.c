bed235fa004d33ac7689f56ad7cd3edd547bf334
JustAbhi29 <justabhi2902@gmail.com>
2023-01-18 10:55:31 +0530

    First Commit

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  printf("Enter the value of a");
+  scanf("%d",&a);
+  printf("Enter the value of b");
+  scanf("%d",&b);
+  printf("Value of a+b = ",c);
+}
